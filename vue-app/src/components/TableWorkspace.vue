eb6b6231ba392f0d7f00cabaacd5f8ca38ae5468
RominaSaravia <61885187+RominaSaravia@users.noreply.github.com>
2024-11-19 23:58:19 +0000

    parent child relationship, add new task and modify them

--- vue-app/src/components/TableWorkspace.vue
+++ vue-app/src/components/TableWorkspace.vue
@@ -1,58 +1,111 @@
 <script setup>
-import { ref, reactive,onMounted } from 'vue'
+import { ref, reactive, onMounted, onBeforeMount, onBeforeUpdate } from 'vue'
 
-defineProps({
-  tableName: {
-    type: String,
-    required: true,
-  },
-  tableItems:{
-    type:Array,
-    required:true,
+// defineProps({
+//     tableName: {
+//         type: String,
+//         required: true,
+//     },
+//     tableItems: {
+//         type: Array,
+//         required: true,
+//     }
+// })
 
-  }
-})
-
-//Filter table row by TableName
-// onMounted(function() {
-//     console.log(this.tableItems);
-//     this.tableItems =  this.tableItems.filter(x => {
-//         x.tableName == 'In Progress'
-//     })
-  
 
 </script>
 
+<script>
+import { reactive } from 'vue'
+
+export default {
+    setup: () => {
+
+    },
+    props: {
+        tableName: {
+            type: String,
+            required: true,
+        },
+        tableItems: {
+            type: Array,
+            required: true,
+        }
+    },
+    data: function () {
+        return {
+            tableItemsToShow: reactive([])
+        }
+    }, components: {},
+    methods: {
+        filterTableItems: function () { // muestra cuál componente emitió el evento "product-click"
+            this.tableItemsToShow = this.tableItems.filter(x => {
+                return x.tableName == this.tableName
+            })
+        },
+        updateTask: function(i){
+            console.log(i)
+
+        }
+    },
+    mounted() {
+        this.filterTableItems();
+        console.log('mounted');
+    },
+    beforeUpdate(){
+        console.log('table call ',this.taskList);
+    }
+
+}
+</script>
+
 <template>
-        <table class="table table-hover">
-            <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">{{tableName}}</th>
-                    <th scope="col">
-                        <button type="button" class="btn btn-outline-success"  v-on:click="$emit('new-task',this.tableName)">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
-                                <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"></path>
-                                <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"></path>
-                            </svg>
-                          </button>
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr  v-for="(item,index) in this.tableItems" v-bind:nombre item.nombre>
-                    <th scope="row">#{{index}}</th>
-                    <td contentEditable="true" >{{item.nombre}}</td>
-                </tr>
-            </tbody>
-        </table>
+    <table class="table table-hover">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">{{ tableName }}</th>
+                <th scope="col">
+                    <button type="button" class="btn btn-outline-success" v-on:click="$emit('new-task', this.tableName)">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                            class="bi bi-plus-square" viewBox="0 0 16 16">
+                            <path
+                                d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z">
+                            </path>
+                            <path
+                                d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4">
+                            </path>
+                        </svg>
+                    </button>
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            
+            <tr v-for="(item, index) in this.tableItems" v-bind:nombre item.nombre>
+                <div v-if="item.tableName == this.tableName" >
+                <th scope="row">#{{ index }}</th>
+                <td>
+                    <input 
+                    type="text" 
+                    name="item.id" 
+                    id="item.id" 
+                    v-model=" item.nombre" 
+                    @change="$emit('update-task', item)">
+                    
+                </td>
+                </div>
+            </tr>
+            
+
+        </tbody>
+    </table>
 </template>
 
 <style scoped>
-.table-container{
+.table-container {
     display: flex;
     flex-direction: row;
 
 }
 </style>
-
--- vue-app/src/components/Workspace.vue
+++ vue-app/src/components/Workspace.vue
@@ -1,47 +1,81 @@
 <script setup>
 import TableWorkspace from './TableWorkspace.vue'
-import { ref, reactive,onMounted  } from 'vue'
+import { ref, reactive,onMounted,onBeforeMount ,onBeforeUpdate } from 'vue'
+
 
 let taskList = reactive([
     {
+        id:"0001",
         nombre:"Tarea a realizar",
         tableName:"Backlog",
     },
     {
+        id:"0002",
         nombre:"Tarea a realizar",
         tableName:"In Progress",
     },
     {
+        id:"0003",
         nombre:"Tarea a realizar",
         tableName:"Finalizadas",
-    },
+    }
 ])
 
+let workTables = reactive(new Set());
 
-function addNewItem(tableName){
-    console.log(tableName);
+
+function addNewItem(e){
     taskList.push({
-        nombre: "nueva tarea"
+        nombre: "nueva tarea",
+        tableName:e,
     });   
 
 }
 
-onMounted(()=> {
-  console.log("Mounted");
-  console.log(taskList)
+function updateItemTable(updatedItem){
+    console.log("updateItemTable", taskList)
+    for (let index = 0; index < taskList.length; index++) {
+        if(taskList[index].id == updatedItem.id)
+        {
+            taskList[index].nombre = updatedItem.nombre;
+            return;
+        }
+        
+    }
 
+}
 
+onBeforeMount(()=> {
+  for (let index = 0; index < taskList.length; index++) {
+    const element = taskList[index];
+    workTables.add(element.tableName);
+    
+  }
+})
+
+onBeforeUpdate(()=> {
+    console.log(this.taskList)
 })
 
 </script>
 
+
 <template>
-    <div class="container table-container">
+    <!-- <div class="container table-container">
     <TableWorkspace tableName = "Backlog" v-bind:tableItems = "taskList" v-on:new-task = "addNewItem"></TableWorkspace>
     <TableWorkspace tableName = "In Progress" v-bind:tableItems = "taskList" v-on:new-task = "addNewItem"></TableWorkspace>
     <TableWorkspace tableName = "Tareas Finalizadas" v-bind:tableItems = "taskList"  v-on:new-task = "addNewItem"></TableWorkspace>
-    </div>
+    </div> -->
 
+    <div class="container table-container">
+    <TableWorkspace v-for="(item,index) in workTables" 
+    v-bind:tableName = item  
+    v-model:tableItems = "taskList" 
+    v-on:new-task = "addNewItem"
+    v-on:update-task = "updateItemTable"
+    ></TableWorkspace>
+
+    </div>
 </template>
 
 <style scoped>
